8bf59fff6e504b617d0bc67cc9467ceffb352b32
dyoshikawa <dyoshikawa1993@gmail.com>
2022-01-15 01:12:21 +0900

    e2e

---
+++ packages/e2e/src/login-api/verify-id-token.test.ts
@@ -0,0 +1,17 @@
+import 'regenerator-runtime/runtime'
+import fetch from 'node-fetch'
+import { CONFIG } from '../config'
+
+test('verify-id-token', async () => {
+  const result = await fetch(
+    new URL('/oauth2/v2.1/verify', CONFIG.BASE_URL).toString(),
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'id_token=DEFAULT_USER&client_id=1234567890',
+    }
+  )
+  expect(result.ok).toBe(true)
+})
