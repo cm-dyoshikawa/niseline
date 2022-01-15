f1dfb8073f78ff496c5472c32aec823d3e745e9a
dyoshikawa <dyoshikawa1993@gmail.com>
2022-01-16 00:30:42 +0900

    init sample-client-app

---
+++ packages/sample-client-app/vite.config.ts
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+})
