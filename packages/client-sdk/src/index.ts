f343f02d8b23b60086ba816078a41772acb34403
dyoshikawa <dyoshikawa1993@gmail.com>
2022-01-15 15:09:05 +0900

    client-sdk

--- packages/client-sdk/src/index.ts
+++ packages/client-sdk/src/index.ts
@@ -1,7 +1,10 @@
 import { Liff } from '@line/liff'
+import sleep from 'sleep-promise'
 
 const clientSdk: Pick<Liff, 'init'> = {
-  init: async () => {},
+  init: async () => {
+    await sleep(1000)
+  },
 }
 
 export default clientSdk
