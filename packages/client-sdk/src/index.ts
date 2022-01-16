ec2ef86362d8cb78dd6b5972447ff635faad68ee
dyoshikawa <dyoshikawa1993@gmail.com>
2022-01-16 17:31:00 +0900

    client-sdk

--- packages/client-sdk/src/index.ts
+++ packages/client-sdk/src/index.ts
@@ -1,13 +1,15 @@
 /* eslint-disable no-console */
 
 import { Liff } from '@line/liff'
+import { v4 as uuidV4 } from 'uuid'
 import { ConsoleLogger, Logger } from './logger'
 
 const logger: Logger = new ConsoleLogger()
+const clientEndpoint = 'http://localhost:3001'
 const authEndpoint = 'http://localhost:3000'
 
 const clientSdk: Pick<Liff, 'init'> = {
-  init: async () => {
+  init: async (config) => {
     logger.info('init')
 
     const accessToken = localStorage.getItem('ACCESS_TOKEN')
@@ -17,10 +19,17 @@ const clientSdk: Pick<Liff, 'init'> = {
     } else if (idToken != null) {
       // TODO
     } else {
-      window.location.href = new URL(
-        '/linely/authorize',
-        authEndpoint
-      ).toString()
+      const state = uuidV4()
+      localStorage.setItem('STATE', state)
+      const url = new URL('/linely/authorize', authEndpoint)
+      url.search = new URLSearchParams({
+        response_type: 'code',
+        client_id: config.liffId,
+        scope: 'default',
+        redirect_uri: clientEndpoint,
+        state,
+      }).toString()
+      window.location.href = url.toString()
     }
   },
 }
