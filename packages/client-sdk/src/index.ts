b1c47213bbc45149d4b4e912ee98f6be391e2cd8
dyoshikawa <dyoshikawa1993@gmail.com>
2022-01-17 02:38:36 +0900

    client-sdk get tokens

--- packages/client-sdk/src/index.ts
+++ packages/client-sdk/src/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
 import { Liff } from '@line/liff'
+import { buildGetAccessToken } from './method/get-access-token'
+import { buildGetIdToken } from './method/get-id-token'
 import { buildInit } from './method/init'
 import { ConsoleLogger, Logger } from './util/logger'
 
@@ -8,6 +10,11 @@ const logger: Logger = new ConsoleLogger()
 const clientEndpoint = window.location.origin
 const authEndpoint = 'http://localhost:3000'
 
-export const buildClientSdk = (): Pick<Liff, 'init'> => ({
+export const buildClientSdk = (): Pick<
+  Liff,
+  'init' | 'getAccessToken' | 'getIDToken'
+> => ({
   init: buildInit({ logger, clientEndpoint, authEndpoint }),
+  getAccessToken: buildGetAccessToken(),
+  getIDToken: buildGetIdToken(),
 })
