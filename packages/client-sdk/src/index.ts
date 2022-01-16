7fd9f9557f9487cca8370b3a2382e1c47e8958d1
dyoshikawa <dyoshikawa1993@gmail.com>
2022-01-16 15:04:48 +0900

    sample-client-app

--- packages/client-sdk/src/index.ts
+++ packages/client-sdk/src/index.ts
@@ -1,8 +1,10 @@
+/* eslint-disable no-console */
 import { Liff } from '@line/liff'
 import sleep from 'sleep-promise'
 
 const clientSdk: Pick<Liff, 'init'> = {
   init: async () => {
+    console.log('@linely/client-sdk: init')
     await sleep(1000)
   },
 }
--- packages/sample-client-app/src/main.tsx
+++ packages/sample-client-app/src/main.tsx
@@ -1,11 +1,18 @@
+import clientSdk from 'client-sdk'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+clientSdk
+  .init({
+    liffId: 'dummy',
+  })
+  .then(() => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    )
+  })
