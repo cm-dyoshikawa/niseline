c9e610aec58b92a70ea1daf05fdbb4a038db614f
dyoshikawa <yoshikawa.daio@classmethod.jp>
2022-01-17 13:32:15 +0900

    fix api path

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -33,7 +33,7 @@ fastify.post(
   container.get<RouteHandlerMethod>(DI_TYPE.DEBUG_REGISTER_USER_HANDLER)
 )
 fastify.get(
-  '/linely/users/me/_access-token',
+  '/linely/users/me/_accessToken',
   container.get<RouteHandlerMethod>(
     DI_TYPE.FIND_USER_BY_ACCESS_TOKEN_FASTIFY_HANDLER
   )
