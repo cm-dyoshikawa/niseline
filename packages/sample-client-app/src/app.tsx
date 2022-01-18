43bcf170b30d3a880c14fcd7927af15811000e01
dyoshikawa <dyoshikawa1993@gmail.com>
2022-01-18 23:06:51 +0900

    sample client app

--- packages/sample-client-app/src/app.tsx
+++ packages/sample-client-app/src/app.tsx
@@ -24,9 +24,17 @@ export const App: React.VFC<{}> = () => {
   return (
     <>
       <div className="flex justify-center p-4">
-        <div className="flex items-stretch border-2 border-gray rounded max-w-md p-2">
-          <img src="" className="rounded-full" />
-          <div className="ml-4 flex items-center">
+        <div className="flex items-stretch p-2 max-w-md rounded border-2 border-slate-700">
+          <img
+            src={user.picture}
+            className="h-20 rounded-full"
+            width={50.8}
+            height={80}
+            onError={(e) => {
+              ;(e.target as any).src = '/img/user-icon.png'
+            }}
+          />
+          <div className="flex items-center ml-4">
             <div>
               <div>id: {user.id}</div>
               <div>name: {user.name}</div>
